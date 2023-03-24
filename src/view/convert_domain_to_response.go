dd598d15e3d22fe9a52867c48f584723d40bcb53
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-03-24 00:12:10 -0300

    refactor: converting domain

---
+++ src/view/convert_domain_to_response.go
@@ -0,0 +1,14 @@
+package view
+
+import "github.com/Falchizao/api-golang/src/controller/model/response"
+
+func ConvertDomainToResponse(
+	userDomain model.UserDomainInterface,
+) response.UserResponse {
+	return response.UserResponse{
+		ID:    userDomain.GetID(),
+		Email: userDomain.GetEmail(),
+		Name:  userDomain.GetName(),
+		Age:   userDomain.GetAge(),
+	}
+}
