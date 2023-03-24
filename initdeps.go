6d01f581f6446ba880ea200ff78d57b79bcdbd7c
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-03-24 00:09:58 -0300

    added init dependencies file

---
+++ initdeps.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func initDependencies(
+	database *mongo.Database,
+) controller.UserControllerInterface {
+	repo := repository.NewUserRepository(database)
+	service := service.NewUserDomainService(repo)
+	return controller.NewUserControllerInterface(service)
+}
