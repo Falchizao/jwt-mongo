771ed7a2a465c3a2cac371e40414658ebad94fae
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-03-23 22:52:42 -0300

    added model binding rules

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
-	Age      int    `json:"age"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=4,max=100"`
+	Name     string `json:"name" binding:"required,min=7,max=20"`
+	Age      int    `json:"age" binding:"required,min=18,max=100"`
 }
