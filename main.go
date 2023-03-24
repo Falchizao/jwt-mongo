8475eb342b67ccd604ef61793cd2c16a869c3b87
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-03-24 00:14:24 -0300

    mongo fetch

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/Falchizao/api-golang/src/controller/routes"
@@ -9,13 +10,26 @@ import (
 )
 
 func main() {
+
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("erro porra")
+		log.Fatal("Erro ao carregar env")
+		return
 	}
 
+	database, err := mongodb.NewMongoDBConnection(context.Background())
+	if err != nil {
+		log.Fatalf(
+			"Error connectando ao banco, error=%s \n",
+			err.Error())
+		return
+	}
+
+	userController := initDependencies(database)
+
 	router := gin.Default()
-	routes.Init(&router.RouterGroup)
+	routes.InitRoutes(&router.RouterGroup, userController)
+
 	if err := router.Run(":8083"); err != nil {
 		log.Fatal(err)
 	}
