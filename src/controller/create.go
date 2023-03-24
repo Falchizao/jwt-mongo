e1fdccc70dc340674dfdc18cc7653dd66526245d
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-03-23 22:46:38 -0300

    create add func

--- src/controller/create.go
+++ src/controller/create.go
@@ -1,11 +1,23 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/Falchizao/api-golang/src/configuration/handling_err"
+	"github.com/Falchizao/api-golang/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := handling_err.NewBadRequestError("Erro endpoint")
-	c.JSON(err.Code, err)
+	var user_req request.UserRequest
+
+	if err := c.ShouldBindJSON(&user_req); err != nil {
+		restErr := handling_err.NewBadRequestError(fmt.Sprintf("Campos incorretos, erro=%s", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(user_req)
+
 }
